67d628eb92eaed20963da990b5c0b34c0e179987
ZancanellaP <matheus.zancanella@estudante.ufjf.br>
2022-02-11 20:44:07 -0300

    Complementando classes

--- src/main/java/com/mycompany/webtutor/Aula.java
+++ src/main/java/com/mycompany/webtutor/Aula.java
@@ -5,10 +5,46 @@
  */
 package com.mycompany.webtutor;
 
+import java.util.Date;
+
 /**
  *
  * @author pedro
  */
 public class Aula {
+    private Materia materia;
+    private Date data;
+    private UsuarioAluno aluno;
+    public Aula(Materia materia,Date data){
+        this.materia = materia;
+        this.data = data;
+        }
+
+    public Materia getMateria() {
+        return materia;
+    }
+
+    public void setMateria(Materia materia) {
+        this.materia = materia;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public UsuarioAluno getAluno() {
+        return aluno;
+    }
+
+    public void setAluno(UsuarioAluno aluno) {
+        this.aluno = aluno;
+    }
     
+    
+    
+   
 }
