3d59a45455ecc8cb3f1bc8df60aa7ffd4843ad26
ZancanellaP <matheus.zancanella@estudante.ufjf.br>
2022-02-11 20:44:26 -0300

    Complementando classes

--- src/main/java/com/mycompany/webtutor/Materia.java
+++ src/main/java/com/mycompany/webtutor/Materia.java
@@ -10,5 +10,39 @@ package com.mycompany.webtutor;
  * @author pedro
  */
 public class Materia {
+    private int id;
+    private String nome;
+    private String descricao;
+    public Materia(int id, String nome, String descricao){
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+                
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    
     
 }
--- src/main/java/com/mycompany/webtutor/Pedido.java
+++ src/main/java/com/mycompany/webtutor/Pedido.java
@@ -10,9 +10,44 @@ package com.mycompany.webtutor;
  * @author pedro
  */
 public class Pedido {
-    String status;
-    // Aluno aluno;
-    String token;
-    // Professor professor;
-    // BuscaProfessor()
+    private String status;
+    private UsuarioAluno aluno;
+    private String token;
+    private UsuarioProfessor professor;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public UsuarioAluno getAluno() {
+        return aluno;
+    }
+
+    public void setAluno(UsuarioAluno aluno) {
+        this.aluno = aluno;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public UsuarioProfessor getProfessor() {
+        return professor;
+    }
+
+    public void setProfessor(UsuarioProfessor professor) {
+        this.professor = professor;
+    }
+ 
+    //resportaProfessor
+    
+    
 }
