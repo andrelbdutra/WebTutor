30c5c29ba2877a7f5ac99e8dded82dee11b03f65
ZancanellaP <matheus.zancanella@estudante.ufjf.br>
2022-02-11 20:51:53 -0300

    Complementando classes

--- src/main/java/com/mycompany/webtutor/Feedback.java
+++ src/main/java/com/mycompany/webtutor/Feedback.java
@@ -10,5 +10,38 @@ package com.mycompany.webtutor;
  * @author pedro
  */
 public class Feedback {
+    int id;
+    double pontuacao;
+    String descricao;
+    public Feedback(double pontuacao, String descricao){
+        this.pontuacao = pontuacao;
+        this.descricao = descricao;
+    
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getPontuacao() {
+        return pontuacao;
+    }
+
+    public void setPontuacao(double pontuacao) {
+        this.pontuacao = pontuacao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+      
     
 }
