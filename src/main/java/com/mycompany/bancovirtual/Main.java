d2900f89c985958fa1d0b67146672caa2587a5b5
andrelbdutra <bazulylbd@gmail.com>
2022-02-11 19:33:26 -0300

    iniciando projeto

---
+++ src/main/java/com/mycompany/bancovirtual/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.bancovirtual;
+
+//Aluno: André Luís Braga Dutra - 201735038
+//Aluno: Wesley de Almeida Cruz - 201765567AC
+//Aluno: Matheus Pedro Zancanella Barboza - 202035005
+
+/**
+ *
+ * @author Pichau
+ */
+public class Main {
+
+    public static void main(String[] args) {
+     
+    }
+}
